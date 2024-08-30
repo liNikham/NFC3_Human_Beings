fe86891de5422b5c92682cb6c760e29c4f9aa142
Adarsh Gupta <csedsadarsh@gmail.com>
2024-08-30 11:04:12 +0530

    added shelter

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Container from "../../components/shared/Container";
 import Banner from "./Banner/Banner";
 import Category from "./category/Category";
@@ -8,26 +9,30 @@ import AboutUs from "./AboutUs/AboutUs";
 import Review from "./review/Review";
 import logo from "../../assets/petadopyWhite.png";
 import ReviewSlider from "./reviewSlider/ReviewSlider";
+
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleSeeAllClick = () => {
+    navigate("/shelter-registration");
+  };
+
   return (
-    // <div className="bg-slate-50">
     <div>
       <Container>
         <div className="relative ">
           <Banner />
         </div>
       </Container>
-      {/* <Container>
-        <div>
-          <img className="lg:w-96 w-60 mx-auto px-10 py-7" src={logo} alt="" />
-        </div>
-      </Container> */}
       <Container>
         <div className="mt-5 rounded-lg flex flex-col w-[100%]  lg:p-10 p-2">
           <Category />
           <div className="flex justify-center items-center lg:m-10 m-2">
-            <button className="backdrop-blur-xl bg-white/60 text-center lg:w-40 p-2 text-white rounded-md hover:bg-slate-50 font-poppins text-base shadow-md">
-              See All
+            <button
+              className="backdrop-blur-xl bg-white/60 text-center lg:w-40 p-2 text-white rounded-md hover:bg-slate-50 font-poppins text-base shadow-md"
+              onClick={handleSeeAllClick}
+            >
+              Register as Shelter
             </button>
           </div>
         </div>
